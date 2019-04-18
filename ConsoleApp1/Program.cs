cb3f0ee256ca8fba39cac0cedd24274f9e6e053f
visualstudio <visualstudio@230-PC41>
2019-04-18 15:36:09 -0400

    Commit at 3:36PM

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,48 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int[] array1 = new int[100];
+            int[] frequency = new int[100];
+            int n, i, j, ctr;
+
+
+            Console.Write("Give the input of number of elements to be stored in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                array1[i] = Convert.ToInt32(Console.ReadLine());
+                frequency[i] = -1;
+            }
+            for (i = 0; i < n; i++)
+            {
+                ctr = 1;
+                for (j = i + 1; j < n; j++)
+                {
+                    if (array1[i] == array1[j])
+                    {
+                        ctr++;
+                        frequency[j] = 0;
+                    }
+                }
+
+                if (frequency[i] != 0)
+                {
+                    frequency[i] = ctr;
+                }
+            }
+            Console.Write("\nThe frequency of all elements of the array : \n");
+            for (i = 0; i < n; i++)
+            {
+                if (frequency[i] != 0)
+                {
+                    Console.Write("{0} occurs {1} times\n", array1[i], frequency[i]);
+                }
+            }
+
+
         }
     }
 }
